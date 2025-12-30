5983da43fdc68e5e807654a041650dd46520b7da
박준석 <parkjunseok@bagjunseog-ui-MacBookPro.local>
2025-12-30 10:03:34 +0900

    hotfix: remove duplicate code

--- src/features/user/apis/my.api.ts
+++ src/features/user/apis/my.api.ts
@@ -27,21 +27,6 @@ export default function myAPI() {
     return response.data;
   };
 
-  const fetchMyPoints = async (): Promise<MyPointsResponseDTO> => {
-    const response = await axiosInstance.get('/my/points');
-    return response.data;
-  };
-
-  const fetchMyOrders = async (): Promise<MyOrdersResponseDTO> => {
-    const response = await axiosInstance.get('/my/orders');
-    return response.data;
-  };
-
-  const fetchMyWish = async (): Promise<MyWishResponseDTO> => {
-    const response = await axiosInstance.get('/my/wish');
-    return response.data;
-  };
-
   return {
     fetchMyProfile,
     fetchMyPoints,
